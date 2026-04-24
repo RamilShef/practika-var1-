eef3ceaddd4cf4b3e0160c639c36130b61e996ac
ramil <ramilshef@yandex.ru>
2026-04-24 23:46:07 +0300

    first commit 1

---
+++ practika/Varyant1/Varyant1/ProizvoditelWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Varyant1
+{
+    /// <summary>
+    /// Логика взаимодействия для ProizvoditelWindow.xaml
+    /// </summary>
+    public partial class ProizvoditelWindow : Window
+    {
+        public ProizvoditelWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Close();
+            mainWindow.Show();
+        }
+
+        private void Profil_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Функционал на стадии разработки");
+        }
+
+        private void Zayavki_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Функционал на стадии разработки");
+        }
+    }
+}
